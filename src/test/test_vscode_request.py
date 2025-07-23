1895734955106626afe8db8f9b4c5b9f2da503f9
guoxiannv <1327247493@qq.com>
2025-07-23 15:45:15 +0800

    defination request

--- src/test/test_vscode_request.py
+++ src/test/test_vscode_request.py
@@ -1,9 +1,21 @@
+# import requests
+
+# data = {
+#     "uri": "/Users/feiyu/Desktop/type-challenges~type-challenges/scripts/toUrl.ts",  # 你的目标文件
+#     "line": 1,   # 行号（从0开始）
+#     "character": 14  # 列号（从0开始）
+# }
+# resp = requests.post("http://localhost:3000/definition", json=data)
+# print("Status:", resp.status_code)
+# print("Result:", resp.json())
+
+
 import requests
 
 data = {
-    "uri": "/Users/feiyu/Desktop/type-challenges~type-challenges/scripts/toUrl.ts",  # 你的目标文件
-    "line": 1,   # 行号（从0开始）
-    "character": 14  # 列号（从0开始）
+    "uri": "/Users/feiyu/Desktop/code/vscode_plugin/mock_vscode_plugin/out/extension.js",  # 你的目标文件
+    "line": 25,
+    "character": 14
 }
 resp = requests.post("http://localhost:3000/definition", json=data)
 print("Status:", resp.status_code)
